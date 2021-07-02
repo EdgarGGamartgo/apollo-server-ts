0863d2ddf27e6a39601f582ccab4fe48b08d75f1
Edgar Gabriel Martinez Gonzalez <edgarggamartgo@gmail.com>
2021-07-01 21:28:35 -0500

    Defining Schema definitions and operations

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,30 @@
 import { ApolloServer } from 'apollo-server';
 
-// 1
+// 1 typeDefs defines our Schema operations
 const typeDefs = `
   type Query {
-    info: String!
+    operationOne: Int!
+    users: [User!]!  
+    user(id: ID!): User
+  }
+  type Mutation {
+    createUser(name: String!): User!
+  }
+  type User {
+    id: ID!
+    name: String!
   }
 `
 
-// 2
+// 2 resolvers defines the execution of our typeDefs
 const resolvers = {
   Query: {
-    info: () => null
+    operationOne: () => 14
   }
 }
 
-// 3
+// 3 server is the actual GRAPHQL SERVER we're building. It needs to be passed an options object where we declare our Schema definitions (typeDefs)
+// and our Schema operations (resolvers).
 export const server = new ApolloServer({
   typeDefs,
   resolvers,
